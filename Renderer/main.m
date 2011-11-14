f7f75ba0f65a144df343253ceca47817dc9ef4ac
Benjamin Gregorski <bfgorski@bfgorski.local>
2011-11-14 00:22:15 -0800

    Initial Commit

---
+++ Renderer/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Renderer
+//
+//  Created by Benjamin Gregorski on 11/14/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
