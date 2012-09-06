548d0fa628d1c447e4ca7473edea309e58cfc9b5
Ben Gregorski <bfgorski@gmail.com>
2012-09-06 01:29:20 -0700

    WIP for Subdivision

---
+++ Common/Subdivision/SubDMeshRenderOptions.h
@@ -0,0 +1,13 @@
+//
+//  SubDMeshRenderOptions.h
+//  BenGRay
+//
+//  Created by Benjamin Gregorski on 8/28/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SubDMeshRenderOptions : NSObject
+
+@end
---
+++ Common/Subdivision/SubDMeshRenderOptions.m
@@ -0,0 +1,13 @@
+//
+//  SubDMeshRenderOptions.m
+//  BenGRay
+//
+//  Created by Benjamin Gregorski on 8/28/12.
+//
+//
+
+#import "SubDMeshRenderOptions.h"
+
+@implementation SubDMeshRenderOptions
+
+@end
